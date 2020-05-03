291e63bca4bf6e0926640aa73ebc05b6793bd97e
Yoshihiko <tera.bcool.4475@gmail.com>
2020-05-03 12:04:04 +0900

    first commit

---
+++ app/src/androidTest/java/jp/starfree/tera98meteor/texteditortest/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package jp.starfree.tera98meteor.texteditortest
+
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("jp.starfree.tera98meteor.texteditortest", appContext.packageName)
+    }
+}
