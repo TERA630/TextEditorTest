48ef32f87a8addef52c8d71fa892f895df0fdf9d
Yoshihiko <tera.bcool.4475@gmail.com>
2020-05-07 15:15:14 +0900

    list make

--- app/src/main/java/jp/starfree/tera98meteor/texteditortest/FirstFragment.kt
+++ app/src/main/java/jp/starfree/tera98meteor/texteditortest/FirstFragment.kt
@@ -7,12 +7,12 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_first.*
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class FirstFragment : Fragment() {
-        private lateinit var mAdaptor: FirstListAdaptor
         companion object {
             @JvmStatic
             fun newInstance(): FirstFragment {
@@ -21,15 +21,14 @@ class FirstFragment : Fragment() {
         }
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
-    ): View? {
+            savedInstanceState: Bundle? ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_first, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        firstList.adapter = FirstListAdaptor()
         view.findViewById<Button>(R.id.button_first).setOnClickListener {
             findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
         }
--- app/src/main/java/jp/starfree/tera98meteor/texteditortest/FirstListAdaptor.kt
+++ app/src/main/java/jp/starfree/tera98meteor/texteditortest/FirstListAdaptor.kt
@@ -4,16 +4,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.item_simple.view.*
 
 class FirstListAdaptor ():RecyclerView.Adapter<RecyclerView.ViewHolder>() {
-    override fun getItemCount(): Int = 7
+    override fun getItemCount(): Int = 12
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.list_row, parent, false)
-        return FirstListAdaptor.ViewHolderOfCell(itemView)
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.item_simple, parent, false)
+        return ViewHolderOfCell(itemView)
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val VH = holder as ViewHolderOfCell
+        VH.rowView.list_item.text = "我が輩は猫である｡"
     }
     class ViewHolderOfCell(val rowView: View) : RecyclerView.ViewHolder(rowView)
 }
